0bb009793d80ab0108ffc11beba3736991fb714d
Jun Inton <juninton@gmail.com>
2024-10-10 16:52:56 -0700

    tried displaying google map in react rather than static html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-async function initMap() {
-    console.log("Maps JavaScript API loaded.");
-  }
+import React from "react";
+import { createRoot} from "react-dom/client";
+import App from './App.jsx';
+
+// console.log("Hello world");
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<App />);
+
+// async function initMap() {
+//     console.log("Maps JavaScript API loaded.");
+// }
   
-  window.initMap = initMap;
+// window.initMap = initMap;
